3ae5a0966734392b0c6dced7a4bcdbcc78ff9c2e
Mario Righi <lonemario@gmail.com>
2025-08-21 16:50:57 +0200

    Add founding info, add reference to README.MD file and use github tag for version

--- LT.RandomGen.TestConsole/Program.cs
+++ LT.RandomGen.TestConsole/Program.cs
@@ -8,9 +8,9 @@ namespace LT.RandomGen.TestConsole
         {
             for (int i = 0; i < 100; i++)
             {
-                Console.WriteLine(i.ToString()+RandomGenerator.GenerateText(Data.Kinds.Things, 0,Data.Languages.Italian));
+                Console.WriteLine($"{ i.ToString()} - {RandomGenerator.GenerateText(Data.Kinds.Things, 10,Data.Languages.Italian)}");
             }
-            Console.ReadKey();
+            //Console.ReadKey();
         }
     }
 }
